3f0518681aff48b1cf2a6cb994f1130ff5ba6ed0
Sakiba <109652552+Sakinadi21@users.noreply.github.com>
2024-01-14 00:31:47 +0600

    Light more.cpp

---
+++ Light more.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+#include<math.h>
+
+int main()
+
+{
+
+    long int nthBulb,root;
+
+    while(scanf("%lld", &nthBulb) != EOF)
+
+    {
+
+        if(nthBulb==0)
+
+        {
+
+            break;
+
+        }
+
+        root=sqrt(nthBulb);
+
+        if(nthBulb==root*root)
+
+        {
+
+            printf("yes\n");
+
+        }
+
+        else
+
+        {
+
+            printf("no\n");
+
+        }
+
+    }
+
+    return 0;
+
+}
